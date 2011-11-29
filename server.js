eedc25dfd2510c6935da081d1faf9466ca48c5d3
Betobaz <pedroalberto007@gmail.com>
2011-11-29 16:50:05 -0600

    segundo comit

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\nApp (betobaz) is running..');
-}).listen(12766);
+}).listen(41245);
